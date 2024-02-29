eebcbc507b3e0bed2865530a677dbd3890038642
NJunY <junyi6608@gmail.com>
2024-02-29 15:19:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#nav-placeholder").load("components/nav.html");
+    // $("#sidebar-placeholder").load("components/sidebar.html");
+    $("#marquee-placeholder").load("components/marquee.html");
+    $("#carousel-placeholder").load("components/carousel.html");
+    $("#game-showcase-placeholder").load("components/game-showcase.html");
+    $("#content-1-placeholder").load("components/content-1.html");
+    $("#content-2-placeholder").load("components/content-2.html");
+    $("#top-10-ad-placeholder").load("components/top-10-ad.html");
+    $("#content-3-placeholder").load("components/content-3.html");
+    $("#content-4-placeholder").load("components/content-4.html");
+    $("#content-5-placeholder").load("components/content-5.html");
+    $("#content-6-placeholder").load("components/content-6.html");
+    $("#qna-placeholder").load("components/qna.html");
+    $("#carousel-2-placeholder").load("components/carousel-2.html");
+    $("#content-7-placeholder").load("components/content-7.html");
+    $("#carousel-3-placeholder").load("components/carousel-3.html");
+    $("#content-8-placeholder").load("components/content-8.html");
+    $("#content-9-placeholder").load("components/content-9.html");
+    $("#content-10-placeholder").load("components/content-10.html");
+    $("#content-11-placeholder").load("components/content-11.html");
+    $("#content-12-placeholder").load("components/content-12.html");
+    $("#footer-placeholder").load("components/footer.html")
+});
